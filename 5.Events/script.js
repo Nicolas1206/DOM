0de8983673254bbcad53788d665fd854196a3c36
Nicolas Mateo Uroz <nicolasmateo1995@gmail.com>
2023-06-24 22:33:49 +0200

    chore: fix bug commit

--- 5.Events/script.js
+++ 5.Events/script.js
@@ -1,16 +1,57 @@
 const _initTime = Date.now()
+const newSquare = document.querySelector('.displayedsquare-wrapper');
+const logsContainer = document.querySelector('ul');
+/**
+ * 
+ * @returns 
+ */
+function generateRandomCode() {
+  return '#'+Math.floor(Math.random()*16777215).toString(16).padStart(6,0);
+}
 
+/**
+ * 
+ * @returns 
+ */
 function getElapsedTime(){
   return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
 }
-
+/**
+ * 
+ * @param {*} e 
+ */
 function clickOnSquare(e){
-  console.log(e.target.classList[1])
-  console.log(getElapsedTime())
+  const newDisplayedSquare = document.createElement('div');
+  newDisplayedSquare.classList.add('displayedsquare');
+  newDisplayedSquare.classList.add(e.target.classList[1]);
+
+  newDisplayedSquare.onclick = function(e){
+    alert(e.target.classList[1])
+  }
+  newSquare.appendChild(newDisplayedSquare);
+  let logs = document.createElement('li');
+  logs.textContent = "Created at: " + getElapsedTime() + " and his color is: " + e.target.classList[1];
+  logsContainer.appendChild(logs);
 }
 
 const actionsquares = document.querySelectorAll('.actionsquare')
 for(let actionsquare of actionsquares){
   actionsquare.addEventListener('click', clickOnSquare)
 }
+
+document.body.addEventListener('keyup', (event) =>{
+  if (event.code === "Space"){
+    document.body.style.backgroundColor = generateRandomCode();
+  }else if (event.code === "KeyS"){
+    const newSquare = document.querySelectorAll('.displayedsquare')
+    for (let elem of newSquare){
+      elem.remove();
+    }
+  }else if (event.code === "KeyL"){
+    const newList = document.querySelectorAll('li');
+    for (let elem of newList){
+      elem.remove();
+    }
+  }
+})
 
--- 6.Forms/script.js
+++ 6.Forms/script.js
@@ -1,2 +1,78 @@
-/*
-*/
+let input = document.querySelectorAll('input')
+let span1 = document.getElementById('display-firstname')
+let span2 = document.getElementById('a-hard-truth')
+let password = document.getElementById('pwd')
+let passwordConfirm = document.getElementById('pwd-confirm')
+let toggleDarkMode = document.getElementById('toggle-darkmode')
+let selectAll = document.querySelectorAll('*')
+for (let elem of input){
+    elem.addEventListener('keyup', (e) =>{
+        if (elem.id == 'firstname'){
+            span1.textContent = elem.value
+        }else if (elem.id ='age'){
+            if (elem.value >= 18){
+                span2.style.visibility = 'visible'
+            }else{
+                span2.style.visibility = 'hidden'
+            }
+        }
+    })
+}
+
+/**if (elem.id == 'pwd'){
+    if (password.value.length < 6 && password.value.length >=0){
+        password.style.backgroundColor = 'red'
+    }else{
+        password.style.color = 'black'
+    }
+}**/
+
+password.addEventListener('keyup', () =>{
+    if (password.value.length < 6 && password.value.length >=0){
+        password.style.backgroundColor = 'red'
+    }else{
+        password.style.backgroundColor = 'white'
+    }
+})
+
+passwordConfirm.addEventListener('keyup', () =>{
+    if (passwordConfirm.value != password.value){
+        passwordConfirm.style.background = 'red'
+    }else{
+        passwordConfirm.style.background = 'white'
+    }
+})
+
+toggleDarkMode.addEventListener('change', () =>{
+    if (toggleDarkMode.value === "dark"){
+        document.body.style.backgroundColor = "black"
+        for (let elem of selectAll){
+            elem.style.color = 'white'
+        }
+    }
+    if (toggleDarkMode.value === "light"){
+        document.body.style.backgroundColor = "white"
+        for (let elem of selectAll){
+            elem.style.color = 'black'
+        }
+        let header = document.querySelector('header h1');
+        let footer = document.querySelector('footer');
+        header.style.color = 'white';
+        footer.style.color = 'white';
+    }
+})
+
+/**toggleDarkMode.addEventListener('change', () =>{
+    if (toggleDarkMode.value === "dark"){
+        document.body.style.filter = 'invert(1)'
+        for (let elem of selectAll){
+            elem.style.filter = 'invert(1)'
+        }
+    }
+    if (toggleDarkMode.value === "light"){
+        document.body.style.filter = "invert(0)"
+        for (let elem of selectAll){
+            elem.style.filter = 'invert(0)'
+        }
+    }
+}) */
